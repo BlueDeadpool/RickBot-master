deff946c814fbdb481b9efa27a337f1d5f283a6a
BlueDeadpool <thehivix@gmail.com>
2017-03-12 22:16:41 +0000

    Added !rick

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 const Discord = require('discord.js');
-const client = new Discord.client();
-var token = 'MjkwMzAxNjg1MTYxNzg3Mzkz.C6Y8Bw.H2np-YGnvb2f7IXzQX8fif1VOps';
+const client = new Discord.Client();
 
+function commandIs(str, msg){
+  return msg.content.toLowerCase().startsWith("!" + str);
+}
 
+function pluck(array) {
+  return array.map(function(item) { return item["name"]; });
+}
 
-client.login(token);
+function hasRole(mem, role) {
+  if(pluck(mem.roles).includes(role)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+client.on('ready', () => {
+    console.log('WUBBA LUBBA DUB DUB!');
+});
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/);
+    if(commandIs("ping", message)){
+      message.channel.sendMessage("D-don't ever fucking ping me, Morty.");
+    }
+    if(commandIs("rick", message)){
+      if(hasRole(message.member, "Ricktastic")){
+        if(args.length === 1){
+          message.channel.sendMessage("Say something, you-you little Morty.");
+        } else {
+        message.channel.sendMessage(args.join(" ").substring(5));
+      }
+    }
+  }
+});
+
+client.login('MjkwMzAxNjg1MTYxNzg3Mzkz.C6ZBBA.bCgnY8LWqYf1B9SHO9YH6_ab0tQ');
